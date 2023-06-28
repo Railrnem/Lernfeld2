ebf18609b064b39fd5574761d541709194d8340d
Steffen Robohm <steffen.robohm@gmail.com>
2023-06-28 11:12:44 +0200

    SQL Connect Test

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World")
+import mysql.connector
+
+# Connection-Details
+host="localhost"
+user="root"
+password="1998"
+database="lernfeld2"
+port=3307
+
+# Establish db-connection
+db=mysql.connector.connect(host=host, user=user, password=password, database=database, port=port)
+
+cursor = db.cursor()
+cursor.execute("""
+        SELECT bezeichnung FROM lernfeld2.hardware
+        """)
+result = cursor.fetchall()
+
+print(result)
