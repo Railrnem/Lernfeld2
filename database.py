ba4897adb1dbc2bccb36857ad0f3c8bb45181e73
Steffen Robohm <steffen.robohm@gmail.com>
2023-06-28 12:16:21 +0200

    added ability to add Laptop (without foreign key for now)

--- database.py
+++ database.py
@@ -21,11 +21,19 @@ def printLaptop():
     result = cursor.fetchall()
     return result
 
-def addHardware(descr : str, prop : str, typ : str):
+def addHardware(descr : str):
     db = connectDB()
     cursor = db.cursor()
-    cursor.execute('INSERT INTO lernfeld2.hardware (bezeichnung, eigenschaften, typ) VALUES ("' + descr + '",' + str(prop) + ' ,"' + typ + '")')
+    cursor.execute('INSERT INTO lernfeld2.hardware (bezeichnung) VALUES ("' + descr + '")')
     db.commit()
     cursor.close()
     db.close()
-    
+
+def addLaptop(descr : str, display : str,ram : str,storage : str,batteryhours : str):
+    addHardware(descr)
+    db = connectDB()
+    cursor = db.cursor()
+    cursor.execute('INSERT INTO lernfeld2.laptopeigenschaften (display,ram,storage,batteryhours) VALUES ("' + display + '","' + ram + '","' + storage + '","' + batteryhours + '")')
+    db.commit()
+    cursor.close()
+    db.close()
--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 import database
 
+def addLaptop():
+    print("Enter a name for the laptop:", end=" ")
+    name = input()
+    print()
+    print("Enter the display resolution: ", end=" ")
+    display = input()
+    print()
+    print("Enter how much RAM the Laptop has: ", end=" ")
+    ram = input()
+    print()
+    print("Enter how much storage the Laptop has: ", end=" ")
+    storage = input()
+    print()
+    print("Enter how many hours the battery lasts: ", end=" ")
+    battery = input()
+    print()
+    database.addLaptop(name, display, ram, storage, battery)
+
+
+def addHardware():
+    print("Please enter what kind of Hardware you want to add.")
+    print("Possible Entrys: Laptop")
+    selection = input()
+    if(isinstance(selection, str)):
+        if selection == "Laptop":
+            addLaptop()
+
+
+
 print("Was für eine Hardware brauchst du?")
-print("Mögliche Eingaben: Laptop")
+print("Mögliche Eingaben: Laptop Neu")
 
 selection = input()
 
 if(isinstance(selection, str)):
     if(selection == "Laptop"):
         print(database.printLaptop())
+    if(selection == "Neu"):
+        addHardware()
 else:
     print("Ungültige Eingabe")
