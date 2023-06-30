dc412a1ca1b3e3c96a0ff014f29812c58dfc812d
Steffen Robohm <steffen.robohm@gmail.com>
2023-06-30 11:43:28 +0200

    added form test

--- webseite.py
+++ webseite.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/test/", methods = ['POST', 'GET'])
+def test():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('test.html',form_data = form_data)
+ 
+app.run(host='localhost', port=5000)
