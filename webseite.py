7c2bae7e2c86dc678ee8315cac9989c898571a9f
Steffen Robohm <steffen.robohm@gmail.com>
2023-07-03 10:26:26 +0200

    added Computer Selection

--- webseite.py
+++ webseite.py
@@ -16,6 +16,8 @@ def device():
         form_data = request.form['hardware']
         if form_data.lower() == 'laptop':
             return render_template('laptop.html')
+        if form_data.lower() == 'computer':
+            return render_template('computer.html')
         else:
             return render_template('index.html')
         
@@ -27,8 +29,20 @@ def listlaptop():
         form_data = request.form['part']
         if form_data.lower() == 'display' or form_data.lower() == 'ram' or form_data.lower() == 'storage' or form_data.lower() == 'batteryhours':
             result = database.getLaptopHighScore(form_data.lower())
-            return render_template('list.html', result = result)
+            return render_template('listlaptop.html', result = result)
         else:
             return render_template('laptop.html')
+        
+@app.route("/listcomputer", methods = ['POST', 'GET'])
+def listcomputer():
+    if request.method == 'GET':
+        return f"The URL /device is accessed directly. Try going to '/' to submit form"
+    if request.method == 'POST':
+        form_data = request.form['part']
+        if form_data.lower() == 'cpu' or form_data.lower() == 'gpu' or form_data.lower() == 'ram' or form_data.lower() == 'storage':
+            result = database.getComputerHighScore(form_data.lower())
+            return render_template('listcomputer.html', result = result)
+        else:
+            return render_template('computer.html')
  
 app.run(host='localhost', port=5000)
