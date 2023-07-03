0d6b10b99d22a632e8c31139a379e71b6ab4a98f
Steffen Robohm <steffen.robohm@gmail.com>
2023-07-03 11:04:49 +0200

    removed experimental add hardware code

--- database.py
+++ database.py
@@ -10,34 +10,6 @@ port=3306
 # Establish db-connection
 def connectDB():
     return mysql.connector.connect(host=host, user=user, password=password, database=database, port=port)
-
-# Print Laptop from database
-def printLaptop():
-    db = connectDB()
-    cursor = db.cursor()
-    cursor.execute("""
-        SELECT bezeichnung FROM lernfeld2.hardware
-    """)
-    result = cursor.fetchall()
-    return result
-
-def addHardware(descr : str):
-    db = connectDB()
-    cursor = db.cursor()
-    cursor.execute('INSERT INTO lernfeld2.hardware (bezeichnung) VALUES ("' + descr + '")')
-    db.commit()
-    cursor.close()
-    db.close()
-
-def addLaptop(descr : str, display : str,ram : str,storage : str,batteryhours : str):
-    addHardware(descr)
-    db = connectDB()
-    cursor = db.cursor()
-    # TODO: need to add foreign Key from Hardware
-    cursor.execute('INSERT INTO lernfeld2.laptopeigenschaften (display,ram,storage,batteryhours) VALUES ("' + display + '","' + ram + '","' + storage + '","' + batteryhours + '")')
-    db.commit()
-    cursor.close()
-    db.close()
     
     
 def getLaptopHighScore(category: str):
--- main.py
+++ main.py
@@ -1,33 +1,4 @@
 import database
-
-def addLaptop():
-    print("Enter a name for the laptop:", end=" ")
-    name = input()
-    print()
-    print("Enter the display resolution: ", end=" ")
-    display = input()
-    print()
-    print("Enter how much RAM the Laptop has: ", end=" ")
-    ram = input()
-    print()
-    print("Enter how much storage the Laptop has: ", end=" ")
-    storage = input()
-    print()
-    print("Enter how many hours the battery lasts: ", end=" ")
-    battery = input()
-    print()
-    database.addLaptop(name, display, ram, storage, battery)
-
-
-def addHardware():
-    print("Please enter what kind of Hardware you want to add.")
-    print("Possible Entrys: Laptop")
-    print("Selection:", end=" ")
-    selection = input()
-    print()
-    if(isinstance(selection, str)):
-        if selection == "Laptop":
-            addLaptop()
             
 def evalLaptop():
     print("What is important about the Laptop?")
