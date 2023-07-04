6a06dae68052a758c2f10fd4a04205686b88200c
Steffen Robohm <steffen.robohm@gmail.com>
2023-07-04 10:09:41 +0200

    Added more Tests

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import website
 import unittest
+import database
 
 class WebsiteTests(unittest.TestCase):
 
@@ -9,10 +10,27 @@ class WebsiteTests(unittest.TestCase):
 
     def test_index(self):
         result = self.app.get('/')
+        self.assertIsNotNone(result)
     
     def test_device(self):
         result = self.app.get('/device')
+        self.assertIsNotNone(result)
 
     def test_list(self):
         result = self.app.get('/list')
+        self.assertIsNotNone(result)
+        
+class DatabaseTest(unittest.TestCase):
+
+    def test_computerRead(self):
+        result = database.getComputerHighScore('cpu')
+        self.assertNotEqual(result, '')
+        
+    def test_laptopRead(self):
+        result = database.getLaptopHighScore('display')
+        self.assertNotEqual(result, '')
+            
+    def test_connect(self):
+        result = database.connectDB()
+        self.assertIsNotNone(result)
 
